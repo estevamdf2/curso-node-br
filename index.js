be2a7836391992c4a065ff46dd6c127ea8f5dac3
Marcos Sousa <marcos.sousa@ebc.com.br>
2019-01-03 11:32:17 -0200

    Inicial. Criação do projeto e A3 mód 1 trabalhando com callback

---
+++ index.js
@@ -0,0 +1,77 @@
+/** Aula  7 modulo 0*/
+// console.log('Hello World');
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+/**
+ * Aula 3 módulo 1
+ * 
+ * 0 - obter o usuário
+ * 1 obter o número de telefon de um usuário a partir do seu ID.
+ * 2 obter o endereço do usuário pelo ID
+ */
+
+ function obterUsuario(callback){
+    //Simular um acesso ao BD dando timeout
+    setTimeout(function(){
+        return callback(null, {
+            id: 1,
+            nome: 'Andreia',
+            dataNascimento: new Date()
+        })
+    }, 2000)
+
+ }
+
+ function obterTelefone(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null,{
+            numero: '999990000',
+            ddd: '11'
+        })
+    }, 3000);
+ }
+
+ function obterEndereco(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null, {
+            rua: 'Rua X quadra 10',
+            numero: 10
+        })
+    }, 1000);
+ }
+
+ function resolverUsuario(erro, usuario){
+     console.log('usuario',usuario);
+ }
+//  const usuario = obterUsuario();
+obterUsuario(function resolverUsuario(error, usuario){
+    if(error){
+        console.error("problema em usuario ",error);
+        return;
+    }
+    obterTelefone(usuario.id, function resolverTelefone(error1,telefone){
+        if(error1){
+            console.error("problema em telefone ",error1);
+            return;
+        }
+
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco){
+            if(error2){
+                console.error("problema em endereço ",error2);
+                return;
+            }
+
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereco: ${endereco.rua}, ${endereco.numero},
+                Telefone: ${telefone.ddd} - ${telefone.numero}
+            `);
+        })
+
+    })
+});
+ //const telefone = obterTelefone(usuario.id);
+
+ //console.log('usuario',usuario);
+ //console.log('telefone ',telefone);
